dc90f2345f36f668babbe6819d6efc3e05d439fc
Olufemi-Olarewaju <olarewajufemi99@gmail.com>
2023-12-07 18:30:32 -0500

    Bonus charts started

--- static/js/app.js
+++ static/js/app.js
@@ -203,12 +203,85 @@ d3.json(url).then(function(data){
         };
     };
 
+    function initGauge(){
+        // Enter a speed between 0 and 180
+        let level = 10;
+
+        // Trig to calc meter point
+        let degrees = 180 - level,
+        radius = .5;
+        let radians = degrees * Math.PI / 180;
+        let x = radius * Math.cos(radians);
+        let y = radius * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+        let path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+        let trace1 = {
+            type: 'scatter', 
+            x: [0], 
+            y:[0],
+            marker: {size: 28, color:'850000'},
+            showlegend: false,
+            name: 'speed',
+            text: level,
+            hoverinfo: 'text+name'
+        };
+        
+        let trace2 = {
+            values: [20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20],
+            rotation: 90,
+            text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1'],
+            textinfo: 'text',
+            textposition:'inside',    
+            marker: {colors:['rgba(5, 127, 0, .5)', 'rgba(30, 127, 0, .5)', 'rgba(50, 127, 0, .5)',
+                    'rgba(110, 154, 22, .5)', 'rgba(170, 202, 42, .5)',
+                    'rgba(202, 209, 95, .5)', 'rgba(210, 206, 145, .5)',
+                    'rgba(232, 226, 202, .5)', 'rgba(232, 226, 202, .5)',
+                    'rgba(255, 255, 255, 0)',]},
+            // labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+            hoverinfo: 'label',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+        };
+
+
+        let data = [trace1, trace2];
+
+        let layout = {
+            shapes:[{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                    }}],
+            title: '<b>Gauge</b> <br> Speed 0-100',
+            height: 500,
+            width: 500,
+            xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+            yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+        };
+
+        Plotly.newPlot('gauge', data, layout);
+    }
+
     // Call the initBar fuction to display initial Bar Plot
     initBar()
     // Call the initBubble fuction to display initial Bubble Chart
     initBubble();
-    // Call the initMetaData fuction to display initMetaData Demographic Info
+    // Call the initMetaData fuction to display initial metadata for Demographic Info
     initMetaData();
+    // Call the initGauge fuction to display initial wash frequency
+    initGauge();
 });
 
 
